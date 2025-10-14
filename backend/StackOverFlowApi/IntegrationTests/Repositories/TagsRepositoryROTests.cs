2b80d893c64d879b36e3b30f2139598d82f20a1d
Mariusz Szczerba <mariusz.a.szczerba@gmail.com>
2025-10-14 02:06:39 +0200

    test refactoring

---
+++ backend/StackOverFlowApi/IntegrationTests/Repositories/TagsRepositoryROTests.cs
@@ -0,0 +1,55 @@
+using Domain.Entities.StackOverFlow;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Persistence.DbContexts.StackOverFlow;
+using Persistence.Repositories.StackOverFlow;
+
+namespace IntegrationTests.Repositories;
+
+public class TagsRepositoryROTests
+{
+    readonly Random _random = new Random();
+    readonly TagsRepositoryRO _repository;
+
+    public TagsRepositoryROTests()
+    {
+        Environment.SetEnvironmentVariable("TestsVariable", "InMemoryDatabase");
+
+        string dbName = Guid.NewGuid().ToString();
+   
+        StackOverFlowDbContext context = new(new DbContextOptionsBuilder<StackOverFlowDbContext>()
+            .UseInMemoryDatabase(dbName)
+            .Options);
+
+        for (int i = 0; i < 100; ++i)
+            context.Add(Tag.Create(_random.Next(0, 1000000000).ToString(), _random.Next(0, 1000000000)));
+
+        context.SaveChanges();
+
+        StackOverFlowDbContextRO contextRO = new(new DbContextOptionsBuilder<StackOverFlowDbContextRO>()
+            .UseInMemoryDatabase(dbName)
+            .Options);
+
+        _repository = new TagsRepositoryRO(contextRO);
+    }
+
+    [Fact]
+    public async Task ShouldGetTenTags()
+    {
+        var tags = await _repository.GetTagsAsync(1, 10, "Id", false, CancellationToken.None);
+
+
+        tags.Items.Should().HaveCount(10);
+        tags.PageNumber.Should().Be(1);
+        tags.PageSize.Should().Be(10);
+        tags.HasNextPage.Should().BeTrue();
+        tags.TotalCount.Should().Be(100);
+    }
+
+    [Fact]
+    public async Task ShouldCheckHaveData()
+    {
+        var res = await _repository.CheckHaveData(CancellationToken.None);
+        res.Should().BeTrue();
+    }
+}
---
+++ backend/StackOverFlowApi/IntegrationTests/Repositories/TagsRepositoryTests.cs
@@ -0,0 +1,49 @@
+using Domain.Entities.StackOverFlow;
+using FluentAssertions;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Persistence.DbContexts.StackOverFlow;
+using Persistence.Repositories.StackOverFlow;
+
+namespace IntegrationTests.Repositories;
+
+public class TagsRepositoryTests
+{
+    public TagsRepositoryTests()
+    {
+        Environment.SetEnvironmentVariable("TestsVariable", "InMemoryDatabase");
+    }
+
+    [Fact]
+    public async Task ShouldSetTagsWhenGetDate()
+    {
+        Random random = new Random();
+
+        var connection = new SqliteConnection("Data Source=:memory:");
+        await connection.OpenAsync();
+
+        var options = new DbContextOptionsBuilder<StackOverFlowDbContext>()
+            .UseSqlite(connection)
+            .EnableSensitiveDataLogging()
+            .Options;
+
+        using StackOverFlowDbContext context = new(options);
+        context.Database.EnsureCreated();
+
+        var repository = new TagsRepository(context);
+
+        List<Tag> tags = [
+                Tag.Create(random.Next(0, 1000000000).ToString(), random.Next(0, 1000000000)),
+                Tag.Create(random.Next(0, 1000000000).ToString(), random.Next(0, 1000000000))
+            ];
+
+        await repository.SetTagsAsync(tags, CancellationToken.None);
+
+        var dbTags = context.tags;
+
+        dbTags.Should().HaveCount(tags.Count);
+
+        for (int i = 0; i < tags.Count; ++i)
+            dbTags.Where(el => el.Id == tags[i].Id).First().Should().Be(tags[i]);
+    }
+}
